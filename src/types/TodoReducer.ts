38f0ce929c5bc13ec56ba08816aa081e2f1f5b96
oliveiradniel <kadadniel@gmail.com>
2025-01-11 16:18:48 -0300

    chore: add reducer type todo

---
+++ src/types/TodoReducer.ts
@@ -0,0 +1,24 @@
+import { Todo } from './Todo';
+
+export type TodoState = Todo[];
+
+type TodoWithOptionalProps = {
+  [Key in keyof Todo]?: Todo[Key];
+}
+
+type AddTodo = {
+  type: 'add';
+  payload: Todo | Todo[];
+}
+
+type ChangeTodo = {
+  type: 'change';
+  payload: TodoWithOptionalProps & { id: string };
+}
+
+type DeleteTodo = {
+  type: 'delete',
+  payload: { id: string }
+}
+
+export type TodoAction = AddTodo | ChangeTodo | DeleteTodo
