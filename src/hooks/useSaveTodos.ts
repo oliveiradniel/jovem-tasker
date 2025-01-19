776240c11e261e098f0b133202a4557bc2bc1f5f
oliveiradniel <kadadniel@gmail.com>
2025-01-19 16:21:34 -0300

    chore: enable the use of the useSaveTodos hook, but without the crypto library to encrypt the tasks

--- src/hooks/useSaveTodos.ts
+++ src/hooks/useSaveTodos.ts
@@ -1,44 +1,46 @@
-// import { useCallback, useContext, useEffect, useState } from 'react';
-
-// import { TodoContext } from '../contexts/TodoContext';
+import { useCallback, useContext, useEffect, useState } from 'react';
 
 // import { AES, enc } from 'crypto-js';
-// import { Todo } from '../types/Todo';
+
+import { TodoContext } from '../contexts/TodoContext';
+
+import { Todo } from '../types/Todo';
 
 // const SECRET_KEY = import.meta.env.VITE_SECRET_KEY as string;
-// const LOCAL_STORAGE_KEY = 'TODOS_DATA';
+const LOCAL_STORAGE_KEY = 'TODOS_DATA';
 
-// export function useSaveTodos() {
-//   const [gottedInitialData, setGottedInitialData] = useState(false);
+export function useSaveTodos() {
+  const [gottedInitialData, setGottedInitialData] = useState(false);
 
-//   const { state, dispatch } = useContext(TodoContext);
+  const { state, dispatch } = useContext(TodoContext);
 
-//   const handleChangesTodo = useCallback(() => {
-//     const value = AES.encrypt(JSON.stringify(state), SECRET_KEY);
+  const handleChangesTodo = useCallback(() => {
+    // const value = AES.encrypt(JSON.stringify(state), SECRET_KEY);
 
-//     localStorage.setItem(LOCAL_STORAGE_KEY, value.toString());
-//   }, [state]);
+    localStorage.setItem(LOCAL_STORAGE_KEY, state.toString());
+  }, [state]);
 
-//   useEffect(() => {
-//     try {
-//       const todosData = localStorage.getItem(LOCAL_STORAGE_KEY);
+  useEffect(() => {
+    try {
+      const todosData = localStorage.getItem(LOCAL_STORAGE_KEY);
 
-//       if (todosData) {
-//         const bytes = AES.decrypt(todosData, SECRET_KEY);
-//         const decrypted: Todo[] = JSON.parse(bytes.toString(enc.Utf8));
+      if (todosData) {
+        // const bytes = AES.decrypt(todosData, SECRET_KEY);
+        // const decrypted: Todo[] = JSON.parse(bytes.toString(enc.Utf8));
+        const value: Todo[] = JSON.parse(todosData);
 
-//         dispatch({ type: 'add', payload: decrypted });
-//       }
-//     } catch {
-//       alert('Não foi possível obter as tarefas salvas.');
-//     } finally {
-//       setGottedInitialData(true);
-//     }
-//   }, [dispatch]);
+        dispatch({ type: 'add', payload: value });
+      }
+    } catch {
+      alert('Não foi possível obter as tarefas salvas.');
+    } finally {
+      setGottedInitialData(true);
+    }
+  }, [dispatch]);
 
-//   useEffect(() => {
-//     if (!gottedInitialData) return;
-//     handleChangesTodo();
+  useEffect(() => {
+    if (!gottedInitialData) return;
+    handleChangesTodo();
 
-//   }, [handleChangesTodo, gottedInitialData]);
-// }
+  }, [handleChangesTodo, gottedInitialData]);
+}
