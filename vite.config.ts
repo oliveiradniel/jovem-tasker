cb9b6954477cd174fb43ead180b9c3d0877a67f0
oliveiradniel <kadadniel@gmail.com>
2025-01-19 15:14:49 -0300

    fix: change the vite configuration to generate the index.html file in the build

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: './src/index.tsx'
-    },
-  },
   plugins: [
     react(),
     svgr(),
